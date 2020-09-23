e59b617ce0cf267887ad6157015dce763b25a4cb
st085318 <st085318@student.spbu.ru>
2020-09-23 22:06:19 +0300

    edit power and sqrt add tests

--- main.py
+++ main.py
@@ -1,40 +1,98 @@
 # function output k in natural degree
 
-def power(k, n):
-    try:
-        n = int(n)
-        if n < 0:
-            return -1
-        if k != 0:
-            if n == 0:
-                return 1
-            elif n < 0:
-                return 1 / power(k, -n)
-            else:
-                return k * power(k, n - 1)
-        else:
-            return 0
-    except BaseException:
-        return -1
+class Calculator:
+    def __init__(self, init_value=0):
+        self.value = init_value
 
-
-def sqrt(n, l=1, r=0):
-    try:
-        if n < 0:
+    def add(self, *args):
+        try:
+            self.value += sum(args)
+            return self
+        except BaseException:
             return -1
-        elif 1 > n > 0:
-            return 1 / sqrt(1 / n)
-        else:
-            if r == 0:
-                r = n
-            while r - l > 0.00000000000001:
-                if (r + l) / 2 * (r + l) / 2 < n:
-                    return sqrt(n, (r + l) / 2, r)
-                elif (r + l) / 2 * (r + l) / 2 > n:
-                    return sqrt(n, l, (r + l) / 2)
+
+    def multiply(self, *args):
+        try:
+            for x in args:
+                self.value *= x
+            return self
+        except BaseException:
+            return -1
+
+    def divide(self, *args, integer_divide=False):
+        try:
+            for x in args:
+                if integer_divide:
+                    self.value //= x
                 else:
-                    return (r + l) / 2
-            return r
-    except BaseException:
-        return -1
+                    self.value /= x
+            return self
+        except ZeroDivisionError:
+            return -1
+        except BaseException:
+            return -1
+
+    def power(self, n):
+        try:
+            value = self.value
+            n = int(n)
+            if self.value != 0:
+                if n == 0:
+                    self.value = 1
+                elif n < 0:
+                    calc = Calculator(self.value)
+                    self.value = 1 / calc.power(-n).value
+                else:
+                    for i in range(n-1):
+                        self.value = self.value * value
+                return self
+        except BaseException:
+            self.value = value
+            return self
+
+    def sqrt(self):
+        try:
+            if self.value < 0:
+                return self
+            elif 1 > self.value > 0:
+                l = 0
+                r = 1
+            else:
+                r = self.value
+                l = 0
+            while r - l > 0.000000000001:
+                if (r + l) / 2 * (r + l) / 2 < self.value:
+                    l = (r+l)/2
+                elif (r + l) / 2 * (r + l) / 2 > self.value:
+                    r = (r+l)/2
+                else:
+                    self.value = (r+l)/2
+                    return self
+            self.value = (r+l)/2
+            return self
+        except BaseException:
+            return -1
+
+    def subtract(self, *args):
+        self.value -= sum(args)
+        return self
+
+    def __repr__(self):
+        return self.value
+
+    def __str__(self):
+        return str(self.value)
+
+
+
+
+if __name__ == '__main__':
+    calculator = Calculator(2)
+    #print(calculator)
+    #print(calculator.add(1, 2, 3, 5.1).multiply(4, 0.123).subtract(4, 1, -100).divide(5, integer_divide=True))
+    print(calculator.power(-2).value)
+    #print(Calculator(100) + 10)
+    #print(10 + Calculator(12))
+    #print(Calculator(123) - Calculator(14))
+    #print(Calculator(14) / Calculator(2))
 
--- test.py
+++ test.py
@@ -1,31 +1,99 @@
 import unittest
 from main import *
 import math
+from datetime import datetime
+import random
 
 
-class MyTestCase(unittest.TestCase):
+class TestCalculator(unittest.TestCase):
+    def setUp(self):
+        a = random.randint(1, 10000)
+        self.calculator = Calculator(a)
+
+    def test_random(self):
+        print(datetime.now())
+        calc_value = self.calculator.value
+        self.assertEqual(self.calculator.add(1, 2, 3).value, calc_value + 6)
+        calc_value = self.calculator.value
+        self.assertEqual(self.calculator.multiply(5, 2, 90).value, calc_value * 900)
+        calc_value = self.calculator.value
+        self.assertEqual(self.calculator.divide(2, 3).value, calc_value / 6)
+        calc_value = self.calculator.value
+        self.assertEqual(self.calculator.divide(2, 3).value, calc_value / 6)
+        calc_value = self.calculator.value
+        self.assertAlmostEqual(self.calculator.sqrt().value, math.sqrt(calc_value))
+        calc_value = self.calculator.value
+        self.assertEqual(self.calculator.power(2).value, math.pow(calc_value, 2))
+        calc_value = self.calculator.value
+        self.assertEqual(self.calculator.add(1).multiply(9).divide(3).power(2).value, (calc_value+1)*(calc_value+1)*9)
+
+    def test_add(self):
+        calc_value = self.calculator.value
+        self.assertEqual(self.calculator.add(1, 2, 3).value, calc_value + 6)
+        self.assertRaises(BaseException, self.calculator.add("adad"))
+        self.calculator.value = "12"
+        self.assertRaises(BaseException, self.calculator.add(1))
+
+    def test_add_type(self):
+        self.assertRaises(BaseException, self.calculator.add("happy"))
+        self.calculator.value = "sad"
+        self.assertRaises(BaseException, self.calculator.add(1))
+
+    def test_mul(self):
+        calc_value = self.calculator.value
+        self.assertEqual(self.calculator.multiply(5, 2, 90).value, calc_value * 900)
+
+    def test_mul_type(self):
+        self.assertRaises(BaseException, self.calculator.multiply("happy"))
+        self.calculator.value = "sad"
+        self.assertRaises(BaseException, self.calculator.multiply(1))
+
+    def test_divide(self):
+        calc_value = self.calculator.value
+        self.assertEqual(self.calculator.divide(2, 3).value, calc_value / 6)
+
+    def test_divide_type(self):
+        self.assertRaises(BaseException, self.calculator.divide("happy"))
+        self.calculator.value = "sad"
+        self.assertRaises(BaseException, self.calculator.divide(1))
+
+    def test_divide_zero(self):
+        self.assertRaises(BaseException, self.calculator.divide(0))
+
     def test_sqrt_normal(self):
-        self.assertAlmostEqual(sqrt(0.25), 0.5)
-        self.assertAlmostEqual(sqrt(4), 2)
-        self.assertAlmostEqual(sqrt(13), math.sqrt(13))
-        self.assertEqual(sqrt(-0.25), -1)
+        calc_value = self.calculator.value
+        self.assertAlmostEqual(self.calculator.sqrt().value, math.sqrt(calc_value))
+        self.calculator.value = 0.25
+        self.assertAlmostEqual(self.calculator.sqrt().value, 0.5)
+        self.calculator.value = 4
+        self.assertAlmostEqual(self.calculator.sqrt().value, 2)
+        self.calculator.value = 13
+        self.assertAlmostEqual(self.calculator.sqrt().value, math.sqrt(13))
+        self.calculator.value = -0.25
+        self.assertEqual(self.calculator.sqrt().value, -0.25)
 
     def test_sqrt_type(self):
-        self.assertRaises(BaseException, sqrt("gf"))
-
-    def test_sqrt_break_rec(self):
-        self.assertRaises(BaseException, sqrt(5785923047))
+        self.calculator.value = "sd"
+        self.assertRaises(BaseException, self.calculator.sqrt())
 
     def test_power_normal(self):
-        self.assertEqual(power(1, 10), 1)
-        self.assertEqual(power(123, -5), -1)
-        self.assertEqual(power(12, 2.5), 144)
-        self.assertEqual(power(0.5, 2), 0.25)
+        self.calculator.value = 1
+        self.assertEqual(self.calculator.power(10).value, 1)
+        self.calculator.value = 2
+        self.assertEqual(self.calculator.power(-2).value, 0.25)
+        self.calculator.value = 12
+        self.assertEqual(self.calculator.power(2.5).value, 144)
+        self.calculator.value = 0.5
+        self.assertEqual(self.calculator.power(2).value, 0.25)
 
     def test_power_type(self):
-        self.assertRaises(BaseException, power(12, "two"))
-        self.assertRaises(BaseException, power("one", 13))
+        self.calculator.value = 12
+        self.assertRaises(BaseException, self.calculator.power("two"))
+        self.calculator.value = "one"
+        self.assertRaises(BaseException, self.calculator.power(13))
+        print(datetime.now())
 
 
 if __name__ == '__main__':
     unittest.main()
+
